af838a09ab122e11bc6529748be5b5f6cffba13f
maulindahn <hanamaulinda@gmail.com>
2017-01-11 15:25:08 +0700

    helpandsetting

---
+++ app/src/main/java/com/example/hana/rentcostumes/HelpSIW.java
@@ -0,0 +1,16 @@
+package com.example.hana.rentcostumes;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Annisa on 1/10/2017.
+ */
+
+public class HelpSIW extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.help_siw);
+    }
+}
